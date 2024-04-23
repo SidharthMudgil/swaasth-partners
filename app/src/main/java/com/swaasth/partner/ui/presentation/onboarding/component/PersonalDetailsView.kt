fba3772a6e18e0e7423e26b6fb8345cae51083de
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-04-23 20:43:07 +0530

    initial implementation of Onboarding screens

--- app/src/main/java/com/swaasth/partner/ui/presentation/onboarding/component/PersonalDetailsView.kt
+++ app/src/main/java/com/swaasth/partner/ui/presentation/onboarding/component/PersonalDetailsView.kt
@@ -1,8 +1,44 @@
 package com.swaasth.partner.ui.presentation.onboarding.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.swaasth.partner.ui.theme.White
 
 @Composable
 fun PersonalDetailsView() {
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(it)
+        ) {
 
+            Spacer(modifier = Modifier.weight(1f))
+            
+            Button(
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {}
+            ) {
+                Text(
+                    text = "Next",
+                    fontWeight = FontWeight.SemiBold,
+                    color = White,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/swaasth/partner/ui/presentation/onboarding/component/QualificationDetailsView.kt
+++ app/src/main/java/com/swaasth/partner/ui/presentation/onboarding/component/QualificationDetailsView.kt
@@ -1,8 +1,68 @@
 package com.swaasth.partner.ui.presentation.onboarding.component
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBackIosNew
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.swaasth.partner.ui.theme.Blue80
+import com.swaasth.partner.ui.theme.White
 
 @Composable
 fun QualificationDetailsView() {
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(it)
+        ) {
+            TextButton(
+                onClick = {  },
+                contentPadding = PaddingValues(0.dp),
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .defaultMinSize(1.dp, 1.dp),
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.ArrowBackIosNew,
+                    contentDescription = null,
+                    modifier = Modifier.size(14.dp),
+                    tint = Blue80
+                )
+                Text(text = "Back")
+            }
 
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {}
+            ) {
+                Text(
+                    text = "Next",
+                    fontWeight = FontWeight.SemiBold,
+                    color = White,
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    }
 }
