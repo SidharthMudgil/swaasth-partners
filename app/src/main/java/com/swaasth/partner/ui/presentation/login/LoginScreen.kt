9d67eca3e3142b78ef0f58f08ec53432325812e1
Sidharth Mudgil <68889544+SidharthMudgil@users.noreply.github.com>
2024-05-07 22:31:06 +0530

    add country flag in phone number input

--- app/src/main/java/com/swaasth/partner/ui/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/swaasth/partner/ui/presentation/login/LoginScreen.kt
@@ -3,6 +3,7 @@ package com.swaasth.partner.ui.presentation.login
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +17,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -25,7 +25,6 @@ import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.material.icons.outlined.ArrowDropUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
@@ -39,7 +38,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -58,7 +56,7 @@ import com.swaasth.partner.ui.theme.White
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(onClick: () -> Unit) {
-    var prefix by remember { mutableStateOf("+91") }
+    var prefix by remember { mutableStateOf(Pair(R.drawable.uk, "+44")) }
     var expandedState by remember { mutableStateOf(false) }
 
     Scaffold {
@@ -126,52 +124,45 @@ fun LoginScreen(onClick: () -> Unit) {
                         expandedState = !expandedState
                     },
                 ) {
-                    BasicTextField(
-                        value = prefix,
-                        onValueChange = { value -> prefix = value },
-                        readOnly = true,
-                        singleLine = true,
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(12.dp),
                         modifier = Modifier
                             .menuAnchor()
                             .clip(RoundedCornerShape(12.dp))
                             .background(White, RoundedCornerShape(12.dp))
                             .border(1.dp, Grey10, RoundedCornerShape(12.dp))
                             .padding(10.dp),
-                        decorationBox = {
-                            Row(
-                                horizontalArrangement = Arrangement.spacedBy(12.dp)
-                            ) {
-                                Text(
-                                    text = prefix,
-                                    color = Grey40,
-                                    fontSize = 18.sp,
-                                    modifier = Modifier.background(Color.Transparent)
-                                )
+                    ) {
+                        Image(
+                            painter = painterResource(id = prefix.first),
+                            contentDescription = null,
+                            modifier = Modifier.size(28.dp)
+                        )
 
-                                Icon(
-                                    imageVector = if (!expandedState) Icons.Outlined.ArrowDropDown else Icons.Outlined.ArrowDropUp,
-                                    contentDescription = null
-                                )
-                            }
-                        }
-                    )
+                        Icon(
+                            imageVector = if (!expandedState) Icons.Outlined.ArrowDropDown else Icons.Outlined.ArrowDropUp,
+                            contentDescription = null
+                        )
+                    }
 
                     ExposedDropdownMenu(
                         expanded = expandedState,
                         onDismissRequest = { expandedState = false },
                         modifier = Modifier.fillMaxWidth(),
                     ) {
-                        listOf("+91", "+92").forEach { v ->
-                            
-                            DropdownMenuItem(
-                                text = {
-                                    Text(text = v)
-                                },
-                                onClick = {
-                                    prefix = v
-                                    expandedState = false
-                                },
-                                modifier = Modifier.fillMaxWidth()
+                        listOf(
+                            Pair(R.drawable.qatar, "+974"),
+                            Pair(R.drawable.uk, "+44"),
+                            Pair(R.drawable.hongkong, "+852"),
+                        ).forEach { v ->
+                            Image(
+                                painter = painterResource(id = v.first),
+                                contentDescription = null,
+                                modifier = Modifier.size(48.dp, 36.dp)
+                                    .clickable {
+                                        prefix = v
+                                        expandedState = false
+                                    }
                             )
                         }
                     }
@@ -183,7 +174,7 @@ fun LoginScreen(onClick: () -> Unit) {
                 ) {
                     InputField(
                         hint = "Enter phone number",
-                        prefix = prefix,
+                        prefix = prefix.second,
                         onValueChange = {
 
                         },
--- app/src/main/java/com/swaasth/partner/ui/presentation/onboarding/QualificationDetailsScreen.kt
+++ app/src/main/java/com/swaasth/partner/ui/presentation/onboarding/QualificationDetailsScreen.kt
@@ -1,7 +1,9 @@
 package com.swaasth.partner.ui.presentation.onboarding
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,13 +16,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.material.icons.outlined.ArrowDropUp
 import androidx.compose.material.icons.rounded.ArrowBackIosNew
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.Icon
@@ -36,17 +36,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.swaasth.partner.R
 import com.swaasth.partner.ui.component.DropdownInput
 import com.swaasth.partner.ui.component.InputField
 import com.swaasth.partner.ui.presentation.onboarding.component.PagerIndicator
 import com.swaasth.partner.ui.theme.Blue80
 import com.swaasth.partner.ui.theme.Grey10
-import com.swaasth.partner.ui.theme.Grey40
 import com.swaasth.partner.ui.theme.Grey80
 import com.swaasth.partner.ui.theme.White
 
@@ -56,7 +55,7 @@ fun QualificationDetailsScreen(
     onClick: () -> Unit,
     onBackClick: () -> Unit
 ) {
-    var prefix by remember { mutableStateOf("+91") }
+    var prefix by remember { mutableStateOf(Pair(R.drawable.uk, "+44")) }
     var expandedState by remember { mutableStateOf(false) }
     var expandedState1 by remember { mutableStateOf(false) }
     var selectedItem by remember { mutableStateOf("06:00 pm-07:00 pm") }
@@ -114,6 +113,7 @@ fun QualificationDetailsScreen(
             InputField(
                 hint = "Search for clinic location", onValueChange = { }, outlined = true
             )
+
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.fillMaxWidth()
@@ -124,51 +124,45 @@ fun QualificationDetailsScreen(
                         expandedState = !expandedState
                     },
                 ) {
-                    BasicTextField(
-                        value = prefix,
-                        onValueChange = { value -> prefix = value },
-                        readOnly = true,
-                        singleLine = true,
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(12.dp),
                         modifier = Modifier
                             .menuAnchor()
                             .clip(RoundedCornerShape(12.dp))
                             .background(White, RoundedCornerShape(12.dp))
                             .border(1.dp, Grey10, RoundedCornerShape(12.dp))
                             .padding(10.dp),
-                        decorationBox = {
-                            Row(
-                                horizontalArrangement = Arrangement.spacedBy(12.dp)
-                            ) {
-                                Text(
-                                    text = prefix,
-                                    color = Grey40,
-                                    fontSize = 18.sp,
-                                    modifier = Modifier.background(Color.Transparent)
-                                )
+                    ) {
+                        Image(
+                            painter = painterResource(id = prefix.first),
+                            contentDescription = null,
+                            modifier = Modifier.size(28.dp)
+                        )
 
-                                Icon(
-                                    imageVector = if (!expandedState) Icons.Outlined.ArrowDropDown else Icons.Outlined.ArrowDropUp,
-                                    contentDescription = null
-                                )
-                            }
-                        }
-                    )
+                        Icon(
+                            imageVector = if (!expandedState) Icons.Outlined.ArrowDropDown else Icons.Outlined.ArrowDropUp,
+                            contentDescription = null
+                        )
+                    }
 
                     ExposedDropdownMenu(
                         expanded = expandedState,
                         onDismissRequest = { expandedState = false },
                         modifier = Modifier.fillMaxWidth(),
                     ) {
-                        listOf("+91", "+92").forEach { v ->
-                            DropdownMenuItem(
-                                text = {
-                                    Text(text = v)
-                                },
-                                onClick = {
-                                    prefix = v
-                                    expandedState = false
-                                },
-                                modifier = Modifier.fillMaxWidth()
+                        listOf(
+                            Pair(R.drawable.qatar, "+974"),
+                            Pair(R.drawable.uk, "+44"),
+                            Pair(R.drawable.hongkong, "+852"),
+                        ).forEach { v ->
+                            Image(
+                                painter = painterResource(id = v.first),
+                                contentDescription = null,
+                                modifier = Modifier.size(48.dp, 36.dp)
+                                    .clickable {
+                                        prefix = v
+                                        expandedState = false
+                                    }
                             )
                         }
                     }
@@ -180,7 +174,7 @@ fun QualificationDetailsScreen(
                 ) {
                     InputField(
                         hint = "Enter phone number",
-                        prefix = prefix,
+                        prefix = prefix.second,
                         onValueChange = {
 
                         },
